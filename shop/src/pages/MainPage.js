2c7a830fc9566ff849bd58005fcd548bdbfc81df
kkahlua <csw93083@naver.com>
2022-08-18 23:44:41 +0900

    정렬 기능 삭제, 상품 추가시 경고창

--- shop/src/pages/MainPage.js
+++ shop/src/pages/MainPage.js
@@ -1,52 +1,41 @@
 import Product from "../components/Product";
-import { Col, Row, ToggleButton } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 import "../style/mainPage.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 function MainPage(props) {
-  let [sortSwitch, setSortSwitch] = useState(false);
-  let [dataSwitch, setDataSwitch] = useState(false);
-  let temp = [...props.shoes];
-  temp.sort((a, b) => {
-    return a.price - b.price;
-  });
+  let [dataSwitch, setDataSwitch] = useState(1);
+  let [alert, setAlert] = useState(false);
   useEffect(() => {
-    if (dataSwitch === true) {
+    if (dataSwitch > 1 && dataSwitch < 4) {
       axios
-        .get("https://codingapple1.github.io/shop/data2.json")
+        .get(`https://codingapple1.github.io/shop/data${dataSwitch}.json`)
         .then((result) => {
           result.data.map((a) => {
-            props.shoes.push(a);
+            return props.shoes.push(a);
           });
           console.log(props.shoes);
         })
         .catch(() => {
           console.log("실패함");
         });
+    } else if (dataSwitch > 3) {
+      setAlert(true);
+      setTimeout(() => {
+        setAlert(false);
+      }, 2000);
     }
-  }, [dataSwitch]);
+  }, [dataSwitch, props.shoes]);
   return (
     <div className="mainPage">
       <div className="main-bg"></div>
-      <div className="sortButton">
-        <Row>
-          <Col>
-            <ToggleButton
-              id="toggle-check"
-              type="checkbox"
-              variant="outline-success"
-              checked={sortSwitch}
-              value="1"
-              size="lg"
-              onChange={(e) => setSortSwitch(e.currentTarget.checked)}
-            >
-              가격 낮은 순
-            </ToggleButton>
-          </Col>
-        </Row>
-      </div>
-      <Row>
+      {alert == true ? (
+        <div className="alert alert-danger">
+          상품이 더이상 존재하지 않습니다.
+        </div>
+      ) : null}
+      {/* <Row>
         {props.shoes.map((a, idx) => {
           if (sortSwitch === false)
             return (
@@ -54,10 +43,27 @@ function MainPage(props) {
             );
           else return <Product shoes={temp[idx]} key={temp[idx].title} />;
         })}
-      </Row>
+      </Row> */}
+      {props.shoes.map((a, idx) => {
+        if (idx % 3 === 0)
+          return (
+            <Row>
+              <Product shoes={props.shoes[idx]} key={props.shoes[idx].title} />
+              <Product
+                shoes={props.shoes[idx + 1]}
+                key={props.shoes[idx + 1].title}
+              />
+              <Product
+                shoes={props.shoes[idx + 2]}
+                key={props.shoes[idx + 2].title}
+              />
+            </Row>
+          );
+        else return null;
+      })}
       <button
         onClick={() => {
-          setDataSwitch(true);
+          setDataSwitch(dataSwitch + 1);
         }}
       >
         더보기
