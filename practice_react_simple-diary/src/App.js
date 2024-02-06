a7d0d732f1f35dd4661b6c76c676ed67189d3fd4
kkahlua <csw93083@naver.com>
2024-02-06 19:56:36 +0900

    일기 수정, 삭제 기능구현

--- practice_react_simple-diary/src/App.js
+++ practice_react_simple-diary/src/App.js
@@ -6,6 +6,7 @@ import DiaryList from "./DiaryList";
 function App() {
   let [data, setData] = useState([]);
   let dataId = useRef(0);
+
   function onCreate(author, content, emotion) {
     let created_date = new Date().getTime();
     let newItem = {
@@ -18,10 +19,22 @@ function App() {
     dataId.current += 1;
     setData([newItem, ...data]);
   }
+
+  function onUpdate(targetId, newContent) {
+    setData(
+      data.map((e) => (e.id === targetId ? { ...e, content: newContent } : e))
+    );
+  }
+
+  function onDelete(targetId) {
+    let newDiaryList = data.filter((e) => e.id !== targetId);
+    setData(newDiaryList);
+  }
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} />
+      <DiaryList diaryList={data} onDelete={onDelete} onUpdate={onUpdate} />
     </div>
   );
 }
--- practice_react_simple-diary/src/DiaryItem.jsx
+++ practice_react_simple-diary/src/DiaryItem.jsx
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
-function DiaryItem({ id, author, content, emotion, created_date }) {
+function DiaryItem({
+  id,
+  author,
+  content,
+  emotion,
+  created_date,
+  onDelete,
+  onUpdate,
+}) {
+  let [isEdit, setIsEdit] = useState(false);
+  let [localContent, setLocalContent] = useState(content);
+  let localContentInput = useRef();
+
+  function toggleIsEdit() {
+    setIsEdit(!isEdit);
+  }
+
+  function handleDelete() {
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onDelete(id);
+    }
+  }
+
+  function handleUpdate() {
+    if (localContent.length < 1) {
+      localContentInput.current.focus();
+      return;
+    }
+    if (window.confirm(`${id}번째 일기를 수정하시겠습니까?`)) {
+      onUpdate(id, localContent);
+      toggleIsEdit();
+    }
+  }
+
+  function handelQuitEdit() {
+    setIsEdit(false);
+    setLocalContent(content);
+  }
+
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -10,7 +48,32 @@ function DiaryItem({ id, author, content, emotion, created_date }) {
         <br />
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
-      <div className="content">{content}</div>
+      <div className="contentArea">
+        {isEdit ? (
+          <>
+            <textarea
+              ref={localContentInput}
+              value={localContent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
+      <div className="buttonArea">
+        {isEdit ? (
+          <>
+            <button onClick={handelQuitEdit}>수정 취소</button>
+            <button onClick={handleUpdate}>수정 완료</button>
+          </>
+        ) : (
+          <>
+            <button onClick={handleDelete}>Delete</button>
+            <button onClick={toggleIsEdit}>Update</button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
