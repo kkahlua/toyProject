77b70360f96b350d4e8dd55ab408102e9ce9d8da
kkahlua <csw93083@naver.com>
2024-02-14 20:57:37 +0900

    CSS, Font, Image, 초기Component 설정

--- practice_react_emotion-diary/src/App.js
+++ practice_react_emotion-diary/src/App.js
@@ -4,16 +4,43 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/edit" element={<Edit />} />
-        <Route path="/diary" element={<Diary />} />
-      </Routes>
+      <div className="App">
+        <Header
+          headText={"App"}
+          leftChild={
+            <Button text={"left button"} onClick={() => alert("left click")} />
+          }
+          rightChild={
+            <Button
+              text={"right button"}
+              onClick={() => alert("right click")}
+            />
+          }
+        />
+        <Button
+          text={"button"}
+          onClick={() => alert("Click!")}
+          type={"positive"}
+        />
+        <Button
+          text={"button"}
+          onClick={() => alert("Click!")}
+          type={"negative"}
+        />
+        <Button text={"button"} onClick={() => alert("Click!")} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary/:id" element={<Diary />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
