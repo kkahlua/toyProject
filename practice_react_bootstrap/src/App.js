463bfcb2a886b5bd4fb54432ffdabdd3366542b0
kkahlua <csw93083@naver.com>
2024-02-27 17:52:50 +0900

    notice page 구현

--- practice_react_bootstrap/src/App.js
+++ practice_react_bootstrap/src/App.js
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import EventPage_Bootstrap from "./pages/EventPage_Bootstrap";
+import NoticePage_Bootstrap from "./pages/NoticePage_Bootstrap";
 
 function App() {
-  return <EventPage_Bootstrap />;
+  return (
+    <Routes>
+      <Route path="/" element={<EventPage_Bootstrap />} />
+      <Route path="/notice" element={<NoticePage_Bootstrap />} />
+    </Routes>
+  );
 }
 
 export default App;
--- practice_react_bootstrap/src/components/EventHeader.jsx
+++ practice_react_bootstrap/src/components/EventHeader.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../pages/Event_Bootstrap.module.css";
 import arrow_left from "../assets/icons/arrow_left.svg";
 import share from "../assets/icons/share.svg";
+import { Link } from "react-router-dom";
 
 function EventHeader() {
   return (
@@ -10,9 +11,11 @@ function EventHeader() {
         <img src={arrow_left} alt="왼쪽 화살표" />
       </button>
       <h1 className={styles.header__title}>크리스마스 특별할인</h1>
-      <button className={styles.header__btn}>
-        <img src={share} alt="공유하기" />
-      </button>
+      <Link to={"notice"}>
+        <button className={styles.header__btn}>
+          <img src={share} alt="share button" />
+        </button>
+      </Link>
     </section>
   );
 }
