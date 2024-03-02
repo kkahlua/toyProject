c75ac58d3577589a1215e0fd5b7f89db7ca9657d
kkahlua <csw93083@naver.com>
2024-03-02 21:46:52 +0900

    VideoEdit Page 제작

--- practice_react_bootstrap/src/App.js
+++ practice_react_bootstrap/src/App.js
@@ -4,14 +4,16 @@ import EventPage_Antd from "./pages/EventPage_Antd";
 import EventPage_Bootstrap from "./pages/EventPage_Bootstrap";
 import EventPage_Mui from "./pages/EventPage_Mui";
 import NoticePage_Bootstrap from "./pages/NoticePage_Bootstrap";
+import VideoEditor from "./pages/VideoEditor";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<EventPage_Bootstrap />} />
+      <Route path="/bootstrap" element={<EventPage_Bootstrap />} />
       <Route path="/antd" element={<EventPage_Antd />} />
       <Route path="/mui" element={<EventPage_Mui />} />
       <Route path="/notice" element={<NoticePage_Bootstrap />} />
+      <Route path="/" element={<VideoEditor />} />
     </Routes>
   );
 }
---
+++ practice_react_bootstrap/src/components/VideoPlayer.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Player, BigPlayButton, LoadingSpinner, ControlBar } from "video-react";
+import "video-react/dist/video-react.css";
+
+function VideoPlayer({
+  src,
+  onPlayerChange = () => {},
+  onChange = () => {},
+  startTime = undefined,
+}) {
+  const [player, setPlayer] = useState();
+  const [playerState, setPlayerState] = useState(undefined);
+  const [source, setSource] = useState();
+
+  useEffect(() => {
+    setSource(URL.createObjectURL(src));
+  }, [src]);
+
+  useEffect(() => {
+    if (playerState) {
+      onChange(playerState);
+    }
+  }, [playerState]);
+
+  useEffect(() => {
+    onPlayerChange(player);
+    if (player) {
+      player.subscribeToStateChange(setPlayerState);
+    }
+  }, [player]);
+
+  return (
+    <div className={"video-player"}>
+      <Player
+        src={source}
+        ref={(player) => setPlayer(player)}
+        startTime={startTime}
+      >
+        <source src={source} />
+        <BigPlayButton position="center" />
+        <LoadingSpinner />
+        <ControlBar disableCompletely></ControlBar>
+      </Player>
+    </div>
+  );
+}
+
+export default VideoPlayer;
