5936c1fbbeea2dc3ad35c952a8f59b46daa16e1d
kkahlua <csw93083@naver.com>
2024-03-24 16:39:20 +0900

    검색 기능, 페이지 구현

--- practice_rn_sns/src/Router.jsx
+++ practice_rn_sns/src/Router.jsx
@@ -8,10 +8,23 @@ import Search from './pages/Search';
 import Add from './pages/Add';
 import Play from './pages/Play';
 import MyPage from './pages/MyPage';
+import SearchList from './pages/SearchList';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 const renderTabBar = props => <CustomBottomTab {...props} />;
+
+const SearchTab = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="Search" component={Search} />
+      <Stack.Screen name="SearchList" component={SearchList} />
+    </Stack.Navigator>
+  );
+};
 
 const MainTab = () => {
   return (
@@ -21,7 +34,7 @@ const MainTab = () => {
         headerShown: false,
       }}>
       <Tab.Screen name="홈" component={Home} />
-      <Tab.Screen name="검색" component={Search} />
+      <Tab.Screen name="검색" component={SearchTab} />
       <Tab.Screen name="추가" component={Add} />
       <Tab.Screen name="쇼츠" component={Play} />
       <Tab.Screen name="마이페이지" component={MyPage} />
