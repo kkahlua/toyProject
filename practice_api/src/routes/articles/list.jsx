aa150539286d0be88d4bfd522a1c88d14d5e8cee
kkahlua <csw93083@naver.com>
2024-04-07 03:17:56 +0900

    feat: List 조회

--- practice_api/src/routes/articles/list.jsx
+++ practice_api/src/routes/articles/list.jsx
@@ -1,5 +1,29 @@
+import * as React from "react";
 function List() {
-  return <h1>List</h1>;
+  const [articles, setArticles] = React.useState([]);
+  React.useEffect(() => {
+    fetch("http://localhost:3000/articles", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setArticles(data._embedded.articles);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <>
+      <h1>List</h1>
+      <ul>
+        {articles.map((article, idx) => (
+          <li key={article.id}>{article.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default List;
